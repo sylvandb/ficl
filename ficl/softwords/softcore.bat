b1c6108a246ac2a30a9d30820cd95e05717250df
jsadler <>
2001-04-18 05:38:23 +0000

    added ifbrack.fr

--- ficl/softwords/softcore.bat
+++ ficl/softwords/softcore.bat
@@ -1,1 +1,1 @@
-\perl\bin\perl.exe softcore.pl softcore.fr jhlocal.fr marker.fr oo.fr classes.fr string.fr >..\softcore.c
+\perl\bin\perl.exe softcore.pl softcore.fr jhlocal.fr marker.fr ifbrack.fr oo.fr classes.fr string.fr >..\softcore.c
